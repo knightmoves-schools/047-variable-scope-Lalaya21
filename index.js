b6445b4a700cd1956c406cdb25abd97e38f79086
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-14 18:43:29 +0000

    done1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let distanceInMiles = 100;
+let estimatedArrivalTime = function (){
+    let milesPerHour = 50;
+    distanceInMiles/milesPerHour;
+}
+document.getElementById(result).innerHTML = estimatedArrivalTime ();
+
 
